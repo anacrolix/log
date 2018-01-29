eb919b3464e53accd7c40c5970e28e8705afbd20
Matt Joiner <anacrolix@gmail.com>
2018-01-29 18:06:27 +1100

    Add filters

--- log.go
+++ log.go
@@ -17,8 +17,9 @@ func init() {
 }
 
 type Logger struct {
-	hs     map[Handler]struct{}
-	values map[interface{}]struct{}
+	hs      map[Handler]struct{}
+	values  map[interface{}]struct{}
+	filters map[*Filter]struct{}
 }
 
 func (l *Logger) SetHandler(h Handler) {
@@ -27,14 +28,18 @@ func (l *Logger) SetHandler(h Handler) {
 
 func (l *Logger) Clone() *Logger {
 	ret := &Logger{
-		hs:     make(map[Handler]struct{}),
-		values: make(map[interface{}]struct{}),
+		hs:      make(map[Handler]struct{}),
+		values:  make(map[interface{}]struct{}),
+		filters: make(map[*Filter]struct{}),
 	}
 	for h, v := range l.hs {
 		ret.hs[h] = v
 	}
 	for v, v_ := range l.values {
 		ret.values[v] = v_
+	}
+	for f := range l.filters {
+		ret.filters[f] = struct{}{}
 	}
 	return ret
 }
@@ -48,9 +53,22 @@ func (l *Logger) Handle(m Msg) {
 	for v := range l.values {
 		m.AddValue(v)
 	}
+	for f := range l.filters {
+		if !f.ff(&m) {
+			return
+		}
+	}
 	for h := range l.hs {
 		h.Emit(m)
 	}
+}
+
+func (l *Logger) AddFilter(f *Filter) *Logger {
+	if l.filters == nil {
+		l.filters = make(map[*Filter]struct{})
+	}
+	l.filters[f] = struct{}{}
+	return l
 }
 
 type Msg struct {
@@ -76,6 +94,10 @@ func (msg Msg) Add(key string, value interface{}) Msg {
 func (msg Msg) Log(l *Logger) Msg {
 	l.Handle(msg)
 	return msg
+}
+
+func (m Msg) Values() map[interface{}]struct{} {
+	return m.values
 }
 
 func (m Msg) AddValue(value interface{}) Msg {
