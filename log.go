7418da097baff13c1d7fa4bbd6e9e8af6272b3bc
Matt Joiner <anacrolix@gmail.com>
2018-02-03 11:34:41 +1100

    Move Msg into its own file

--- log.go
+++ log.go
@@ -71,43 +71,6 @@ func (l *Logger) AddFilter(f *Filter) *Logger {
 	return l
 }
 
-type Msg struct {
-	fields map[string][]interface{}
-	values map[interface{}]struct{}
-	text   string
-}
-
-func Fmsg(format string, a ...interface{}) Msg {
-	return Msg{
-		text: fmt.Sprintf(format, a...),
-	}
-}
-
-func (msg Msg) Add(key string, value interface{}) Msg {
-	if msg.fields == nil {
-		msg.fields = make(map[string][]interface{})
-	}
-	msg.fields[key] = append(msg.fields[key], value)
-	return msg
-}
-
-func (msg Msg) Log(l *Logger) Msg {
-	l.Handle(msg)
-	return msg
-}
-
-func (m Msg) Values() map[interface{}]struct{} {
-	return m.values
-}
-
-func (m Msg) AddValue(value interface{}) Msg {
-	if m.values == nil {
-		m.values = make(map[interface{}]struct{})
-	}
-	m.values[value] = struct{}{}
-	return m
-}
-
 type Handler interface {
 	Emit(Msg)
 }
