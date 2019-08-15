d97de38741d08f109d441370cbea8219abeaaec6
Matt Joiner <anacrolix@gmail.com>
2019-08-16 09:59:29 +1000

    Add TestValueStringNonLatin

--- log_test.go
+++ log_test.go
@@ -1,8 +1,12 @@
 package log
 
 import (
+	"bytes"
 	"os"
+	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLogBadString(t *testing.T) {
@@ -10,4 +14,24 @@ func TestLogBadString(t *testing.T) {
 	// var buf bytes.Buffer
 	l.SetHandler(&StreamHandler{W: os.Stderr, Fmt: LineFormatter})
 	Str("\xef\x00\xaa\x1ctest\x00test").AddValue("\x00").Log(&l)
+}
+
+type stringer struct {
+	s string
+}
+
+func (me stringer) String() string {
+	return strconv.Quote(me.s)
+}
+
+func TestValueStringNonLatin(t *testing.T) {
+	s := stringer{"カワキヲアメク\n"}
+	assert.Equal(t, `"カワキヲアメク\n"`, s.String())
+	m := Str("").AddValue(&s)
+	assert.Contains(t, string(LineFormatter(m)), `"カワキヲアメク\n"`)
+	l := new(Logger)
+	var buf bytes.Buffer
+	l.SetHandler(&StreamHandler{&buf, LineFormatter})
+	m.Log(l)
+	assert.Contains(t, buf.String(), `"カワキヲアメク\n"`)
 }
