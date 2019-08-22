148177f3e0fd66530fcb8d2195f8298dece3df2a
Matt Joiner <anacrolix@gmail.com>
2019-08-22 13:09:25 +1000

    Add Print

--- global.go
+++ global.go
@@ -1,6 +1,9 @@
 package log
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 var Default = Logger{StreamLogger{
 	W:   os.Stderr,
@@ -9,4 +12,10 @@ var Default = Logger{StreamLogger{
 
 func Printf(format string, a ...interface{}) {
 	Default.Log(Fmsg(format, a...).Skip(1))
+}
+
+// Prints the arguments to the Default Logger.
+func Print(a ...interface{}) {
+	// TODO: There's no "Print" equivalent constructor for a Msg, and I don't know what I'd call it.
+	Str(fmt.Sprint(a...)).Skip(1).Log(Default)
 }
